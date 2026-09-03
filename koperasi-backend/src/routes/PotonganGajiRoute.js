a25c24e8bb4e2841d8811ab64be07fb398385f31
Hendar Nugraha <manentlabs@gmail.com>
2026-09-03 08:41:33 +0700

    perbaikan routes

--- koperasi-backend/src/routes/PotonganGajiRoute.js
+++ koperasi-backend/src/routes/PotonganGajiRoute.js
@@ -6,11 +6,11 @@ const { verifyToken, checkRole } = require("../middlewares/AuthMiddleware");
 router.use(verifyToken, checkRole("admin", "bendahara"));
 
 router.get("/", PotonganGajiController.index);
-router.post("/", PotonganGajiController.store);          
-router.post("/manual", PotonganGajiController.create);   
-router.get("/potongan-gaji/instansi", PotonganGajiController.listInstansi);
-router.get("/potongan-gaji/anggota-by-instansi", PotonganGajiController.getAnggotaByInstansi);
-router.post("/potongan-gaji/batch", PotonganGajiController.batchStore);
+router.post("/", PotonganGajiController.store);
+router.post("/manual", PotonganGajiController.create);
+router.get("/instansi", PotonganGajiController.listInstansi);
+router.get("/anggota-by-instansi", PotonganGajiController.getAnggotaByInstansi);
+router.post("/batch", PotonganGajiController.batchStore);
 router.put("/:id", PotonganGajiController.update);
 router.post("/:id/process", PotonganGajiController.processToJurnal);
 router.delete("/:id", PotonganGajiController.destroy);
