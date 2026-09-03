6b6f3e26a5942d36fca371b09eb4d15a2845c915
Hendar Nugraha <manentlabs@gmail.com>
2026-09-03 08:26:57 +0700

    perbaikan potongan gaji route

--- koperasi-backend/src/routes/PotonganGajiRoute.js
+++ koperasi-backend/src/routes/PotonganGajiRoute.js
@@ -6,12 +6,12 @@ const { verifyToken, checkRole } = require("../middlewares/AuthMiddleware");
 router.use(verifyToken, checkRole("admin", "bendahara"));
 
 router.get("/", PotonganGajiController.index);
-router.post("/", PotonganGajiController.store);              // import excel
-router.post("/manual", PotonganGajiController.create);       // tambah manual
-router.get("/potongan-gaji/instansi", authMiddleware, potonganGajiController.listInstansi);
-router.get("/potongan-gaji/anggota-by-instansi", authMiddleware, potonganGajiController.getAnggotaByInstansi);
-router.post("/potongan-gaji/batch", authMiddleware, potonganGajiController.batchStore);
-router.put("/:id", PotonganGajiController.update);           // edit manual
+router.post("/", PotonganGajiController.store);          
+router.post("/manual", PotonganGajiController.create);   
+router.get("/potongan-gaji/instansi", PotonganGajiController.listInstansi);
+router.get("/potongan-gaji/anggota-by-instansi", PotonganGajiController.getAnggotaByInstansi);
+router.post("/potongan-gaji/batch", PotonganGajiController.batchStore);
+router.put("/:id", PotonganGajiController.update);
 router.post("/:id/process", PotonganGajiController.processToJurnal);
 router.delete("/:id", PotonganGajiController.destroy);
 
